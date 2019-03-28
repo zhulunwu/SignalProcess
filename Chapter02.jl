fcc5219475fd40b67ab46003996a67717c6219c4
Your Name <you@example.com>
2019-03-28 18:16:46 +0800

    add Chapter02

---
+++ Chapter02.jl
@@ -0,0 +1,31 @@
+using GR
+
+# Unit sample sequence
+function impseq(n0::Int,n1::Int,n2::Int)
+    n=collect(n1:n2)
+    i=findfirst(isequal(n0),n)
+    x=n*0
+    x[i]=1
+    return n,x
+end
+
+function stepseq(n0::Int,n1::Int,n2::Int) 
+    n=collect(n1:n2)
+    idx=findall(e -> e>=n0,n)
+    x = n*0
+    x[idx] .= 1
+    return n,x
+end
+    
+# fig2.1a
+n = -5:5
+x = 2*last(impseq(-2,-5,5)) - last(impseq(4,-5,5))
+stem(n,x)
+xlabel("n"); ylabel("x(n)")
+
+# fig2.1b
+n = 0:20 
+x1 = n.*(last(stepseq(0,0,20))-last(stepseq(10,0,20))) 
+x2 = 10*exp.(-0.3*(n.-10)).*(last(stepseq(10,0,20))-last(stepseq(20,0,20))) 
+x = x1+x2
+stem(n,x)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Digital-Signal-Process
 julia code for "Digital Signal Processing Using MATLAB® Third Edition" by Vinay K. Ingle and John G. Proakis 
+"file://D:\OneDrive\Library\Books\Digital Signal Processing Using MATLAB.pdf"
